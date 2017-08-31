01f09eb9869788babccb974fb47d75415e65c8e2
skanumuri <srikanth.knaumuri@gmail.com>
2017-08-31 16:13:35 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send({ hi: "there" });
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
